fbe746faac654be5caa4626ddce9a4bb2cf293c0
Daniel Rojas <danielrc14@gmail.com>
2018-01-02 02:05:39 -0300

    Until lecture 315

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
+import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <BrowserRouter>
-                    <Layout>
-                        <Switch>
-                            <Route path='/checkout' component={Checkout}/>
-                            <Route path='/orders' component={Orders}/>
-                            <Route path='/' component={BurgerBuilder}/>
-                        </Switch>
-                    </Layout>
-                </BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route path='/checkout' component={Checkout}/>
+                        <Route path='/orders' component={Orders}/>
+                        <Route path='/auth' component={Auth}/>
+                        <Route path='/Logout' component={Logout}/>
+                        <Route path='/' component={BurgerBuilder}/>
+                    </Switch>
+                </Layout>
             </div>
         );
     }
--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,6 +7,10 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' exact>Burger Builder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
+        {props.isAuthenticated ?
+            <NavigationItem link='/logout'>Logout</NavigationItem>
+            : <NavigationItem link='/auth'>Authenticate</NavigationItem>
+        }
     </ul>
 );
 
